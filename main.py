592bf4c390e35cef44de030147ebfe6827da5e9b
flemedias <flemedias@gmail.com>
2025-02-11 10:28:41 -0300

    primeira acao pt6

--- main.py
+++ main.py
@@ -8,9 +8,9 @@ model.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-# @app.get("/")
-# def read_root():
-#  return {"Hello": "lalalalalalalalala"}
+@app.get("/")
+def read_root():
+    return {"Hello": "lala"}
 
 @app.post("/criar", status_code=status.HTTP_201_CREATED)
 def criar_valores(nova_mensagem: classes.Mensagem, db: Session = Depends(get_db)):
@@ -22,6 +22,6 @@ def criar_valores(nova_mensagem: classes.Mensagem, db: Session = Depends(get_db)
     db.refresh(mensagem_criada)
     return {"Mensagem": mensagem_criada}
 
-@app.get("/quadrado/{num}")
-def square(num: int):
-    return num ** 2
+# @app.get("/quadrado/{num}")
+# def square(num: int):
+#     return num ** 2
