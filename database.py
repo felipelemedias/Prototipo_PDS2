a1a3cf994ce578d01b24b727390e6213546a0dd7
flemedias <flemedias@gmail.com>
2025-02-21 09:47:05 -0300

    trying to fix yml

--- database.py
+++ database.py
@@ -8,7 +8,7 @@ if os.getenv("GITHUB_ACTIONS") is None:
 
 user = os.getenv("POSTGRES_USER")
 password = os.getenv("POSTGRES_PASSWORD")
-database = os.getenv("POSTGRES_DB")
+database = os.getenv("POSTGRES_DB") 
 host = os.getenv("POSTGRES_HOST")
 
 SQLALCHEMY_DATABASE_URL = f"postgresql://{user}:{password}@{host}/{database}"
