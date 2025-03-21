ae32e41c23a7c042802b7458e7622bf29baaf0a1
flemedias <flemedias@gmail.com>
2025-03-21 10:16:26 -0300

    agora vai

--- database.py
+++ database.py
@@ -3,12 +3,19 @@ from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-load_dotenv()
+if os.getenv("GITHUB_ACTIONS") is None:
+    load_dotenv(".env.local")
+
+POSTGRES_USER = 'postgres'
+POSTGRES_PASSWORD = 'felipe'
+POSTGRES_DB = 'pdsi2_database'
+POSTGRES_HOST = 'localhost'
 
 database_user = os.getenv("POSTGRES_USER")
 database_name = os.getenv("POSTGRES_DB")
 database_password = os.getenv("POSTGRES_PASSWORD")
 database_host = os.getenv("POSTGRES_HOST")
+
 
 print(f"postgresql://{database_user}:{database_password}@{database_host}/{database_name}")
 
